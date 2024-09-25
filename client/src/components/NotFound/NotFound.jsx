9a79c5573a3d6d2f37b88510d9def6966bddab9d
John Doe <mahdibenalaya566@gmail.com>
2024-09-25 20:17:50 +0100

    prod-server

--- client/src/components/NotFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFound() {
-  return (
-    <p>notfound</p>
-  )
-}
-
-export default NotFound
--- client/src/pages/Admin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Admin() {
-  return (
-    <div>userlist:</div>
-  )
-}
-
-export default Admin
